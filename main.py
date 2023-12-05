44c9f6de77b12c499f308940e15e0b7068e07738
xshapira <max@winoutt.com>
2023-12-05 15:43:58 +0200

    bump ruff to v0.1.7

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main() -> None:
-    pass
+    print("hello world")
 
 
 if __name__ == "__main__":
--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,26 @@
+import time
+
 import streamlit as st
 
 st.title("DocumentGPT")
+
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+
+def send_message(message: str, role: str, save: bool = True) -> None:
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+for message in st.session_state["messages"]:
+    send_message(message["message"], message["role"], save=False)
+
+
+if message := st.chat_input("send a message to the ai"):
+    send_message(message, "human")
+    time.sleep(2)
+    send_message(f"You said: {message}", "ai")
