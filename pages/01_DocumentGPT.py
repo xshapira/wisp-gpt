7ec38cd1e75424da3aa45b129ac64b14cbd675aa
xshapira <max@winoutt.com>
2023-12-06 17:19:27 +0200

    allow users to upload documents and query an AI

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,26 +1,57 @@
-import time
-
 import streamlit as st
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📄",
+)
+
 
 st.title("DocumentGPT")
 
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+st.markdown(
+    """
+    Welcome!
 
+    Use this chatbot to ask questions to an AI about your files.
 
-def send_message(message: str, role: str, save: bool = True) -> None:
-    with st.chat_message(role):
-        st.write(message)
-    if save:
-        st.session_state["messages"].append({"message": message, "role": role})
+    """
+)
 
+if file := st.file_uploader(
+    "Upload a file (.txt, .pdf or .docx)",
+    type=["pdf", "docx", "txt"],
+):
+    st.write(file)
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    st.write(file_content, file_path)
 
-for message in st.session_state["messages"]:
-    send_message(message["message"], message["role"], save=False)
+    with open(file_path, "wb") as fp:
+        fp.write(file_content)
 
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
 
-if message := st.chat_input("send a message to the ai"):
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
+    # llm = ChatOpenAI(
+    #     temperature=0.1,
+    # )
+
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+
+    loader = UnstructuredFileLoader("./files/sample.docx")
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    docs = retriever.invoke("ministry of truth")
+    st.write(docs)
