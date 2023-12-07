0e312b40a8618ee49a3ebdaea97061294361e52e
xshapira <max@winoutt.com>
2023-12-07 16:39:56 +0200

    exclude 'out' directory in gitignore

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,6 +1,9 @@
+from typing import IO
+
 import streamlit as st
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.schema.vectorstore import VectorStore
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
@@ -23,35 +26,33 @@ st.markdown(
     """
 )
 
-if file := st.file_uploader(
-    "Upload a file (.txt, .pdf or .docx)",
-    type=["pdf", "docx", "txt"],
-):
-    st.write(file)
+
+def embed_file(file: IO[bytes]) -> VectorStore:
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
-    st.write(file_content, file_path)
 
     with open(file_path, "wb") as fp:
         fp.write(file_content)
 
     cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
-
-    # llm = ChatOpenAI(
-    #     temperature=0.1,
-    # )
-
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
         chunk_overlap=100,
     )
-
     loader = UnstructuredFileLoader("./files/sample.docx")
     docs = loader.load_and_split(text_splitter=splitter)
     embeddings = OpenAIEmbeddings()
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
-    retriever = vectorstore.as_retriever()
-    docs = retriever.invoke("ministry of truth")
-    st.write(docs)
+    return vectorstore.as_retriever()
+
+
+if file := st.file_uploader(
+    "Upload a file (.txt, .pdf or .docx)",
+    type=["pdf", "docx", "txt"],
+):
+    st.write(file)
+
+    # docs = retriever.invoke("ministry of truth")
+    # st.write(docs)
