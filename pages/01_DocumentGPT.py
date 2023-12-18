fd8f8cafbd8928dd2303711612e58474279b049c
xshapira <max@winoutt.com>
2023-12-18 10:47:12 +0200

    implement memory management and local file storage

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,6 @@
+import json
+from operator import itemgetter
+from pathlib import Path
 from typing import IO
 
 import streamlit as st
@@ -5,7 +8,9 @@ from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.memory import ConversationSummaryBufferMemory
 from langchain.prompts import ChatPromptTemplate
+from langchain.schema import messages_from_dict, messages_to_dict
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.schema.vectorstore import VectorStoreRetriever
 from langchain.storage import LocalFileStore
@@ -32,12 +37,6 @@ st.markdown(
     """
 )
 
-with st.sidebar:
-    file = st.file_uploader(
-        "Upload a file (.txt, .pdf or .docx)",
-        type=["pdf", "docx", "txt"],
-    )
-
 
 class ChatCallbackHandler(BaseCallbackHandler):
     message = ""
@@ -60,6 +59,42 @@ llm = ChatOpenAI(
         ChatCallbackHandler(),
     ],
 )
+
+memory_llm = ChatOpenAI(
+    temperature=0.1,
+)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+
+            Context: {context}
+
+            And you will get a summarized context of the previous chat. If it's empty, you don't have to care.
+
+            Previous-chat-context: {chat_history}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+if "memory" not in st.session_state:
+    st.session_state["memory"] = ConversationSummaryBufferMemory(
+        llm=memory_llm,
+        max_token_limit=120,
+        memory_key="chat_history",
+        return_messages=True,
+    )
+
+
+def load_json(path):
+    with open(path) as fp:
+        return json.load(fp)
 
 
 @st.cache_data(show_spinner="Embedding file...")
@@ -88,6 +123,20 @@ def save_message(message, role):
     st.session_state["messages"].append({"message": message, "role": role})
 
 
+def save_history(input, output):
+    st.session_state["chat_history"].append(
+        {"input": input, "output": output},
+    )
+
+
+def save_history_to_file(history_file_path):
+    history = st.session_state["memory"].chat_memory.messages
+    history = messages_to_dict(history)
+
+    with open(history_file_path, "w") as fp:
+        json.dump(history, fp, indent=2)
+
+
 def send_message(message, role, save=True):
     with st.chat_message(role):
         st.markdown(message)
@@ -104,28 +153,28 @@ def format_docs(docs):
     return "/n/n".join(document.page_content for document in docs)
 
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
-
-            Context: {context}
-            """,
-        ),
-        ("human", "{question}"),
-    ]
-)
+def restore_history_from_memory():
+    for history in st.session_state["chat_history"]:
+        st.session_state["memory"].save_context(
+            {"input": history["input"]},
+            {"output": history["output"]},
+        )
 
 
-if file:
+@st.cache_data(show_spinner="Loading history from file...")
+def load_history_from_file(history_file_path):
+    loaded_message = load_json(history_file_path)
+    history = messages_from_dict(loaded_message)
+    st.session_state["memory"].chat_memory.messages = history
+
+
+def manage_chat_session(file, history_file_path):
     retriever = embed_file(file)
-
     send_message("I'm ready! Ask away!", "ai", save=False)
+    restore_history_from_memory()
     display_chat_history()
-    message = st.chat_input("Ask me any question about your file")
 
+    message = st.chat_input("Ask me any question about your file")
     if message:
         send_message(message, "human")
         chain = (
@@ -133,12 +182,35 @@ if file:
                 "context": retriever | RunnableLambda(format_docs),
                 "question": RunnablePassthrough(),
             }
+            | RunnablePassthrough.assign(
+                chat_history=RunnableLambda(
+                    st.session_state["memory"].load_memory_variables
+                )
+                | itemgetter("chat_history")
+            )
             | prompt
             | llm
         )
         with st.chat_message("ai"):
-            chain.invoke(message)
+            result = chain.invoke(message)
+            save_history(message, result.content)
+
+        if len(st.session_state["memory"].chat_memory.messages) != 0:
+            save_history_to_file(history_file_path=history_file_path)
 
 
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a file (.txt, .pdf or .docx)",
+        type=["pdf", "docx", "txt"],
+    )
+    history_file_path = Path("./.cache/chat_history/history.json")
+    if history_file_path.exists():
+        load_history_from_file(history_file_path)
+
+
+if file:
+    manage_chat_session(file, history_file_path)
 else:
     st.session_state["messages"] = []
+    st.session_state["chat_history"] = []
