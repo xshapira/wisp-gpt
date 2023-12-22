f6de2dc76f683ca0c18d171b41ab567f1e00bd6c
xshapira <max@winoutt.com>
2023-12-22 14:12:27 +0200

    implement standalone functions instead of App class approach

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -9,54 +9,51 @@ from langchain.prompts import (
 from utils import ChatCallbackHandler, ChatModel, intro, load_markdown, load_txt
 
 
-class App:
-    def __init__(self):
-        self.chat_model = ChatModel()
-        self.chat_model.llm = ChatOpenAI(
-            temperature=0.1,
-            streaming=True,
-            callbacks=[
-                ChatCallbackHandler(),
-            ],
-        )
-        self.messages = [
-            SystemMessagePromptTemplate.from_template(
-                load_txt("./prompt_templates/document_gpt/system_message.txt")
-            ),
-            HumanMessagePromptTemplate.from_template("{question}"),
-        ]
-        self.chat_model.prompt = ChatPromptTemplate.from_messages(
-            messages=self.messages
-        )
-        self.chat_model.memory_llm = ChatOpenAI(
-            temperature=0.1,
-        )
+def configure_chat_model():
+    chat_model = ChatModel()
+    chat_model.llm = ChatOpenAI(
+        temperature=0.1,
+        streaming=True,
+        callbacks=[
+            ChatCallbackHandler(),
+        ],
+    )
+    messages = [
+        SystemMessagePromptTemplate.from_template(
+            load_txt("./prompt_templates/document_gpt/system_message.txt")
+        ),
+        HumanMessagePromptTemplate.from_template("{question}"),
+    ]
+    chat_model.prompt = ChatPromptTemplate.from_messages(messages=messages)
+    chat_model.memory_llm = ChatOpenAI(
+        temperature=0.1,
+    )
+    return chat_model
 
-    def run(self):
-        self.chat_model.configure_chat_memory(
-            self.chat_model.memory_llm,
-        )
-        intro_config = {
-            "page_title": "DocumentGPT",
-            "page_icon": "📄",
-            "title": "DocumentGPT",
-            "markdown": load_markdown("./markdowns/document_gpt.md"),
-            "history_file_path": "./.cache/chat_history/history.json",
-            "prompt": self.chat_model.prompt,
-            "llm": self.chat_model.llm,
-            "chat_session_args": {
-                "_file_path": "files",
-                "_cache_dir": "embeddings",
-                "_embeddings": OpenAIEmbeddings(),
-            },
-        }
 
-        intro(**intro_config)
+def run_chat_session(chat_model):
+    chat_model.configure_chat_memory(chat_model.memory_llm)
+    intro_config = {
+        "page_title": "DocumentGPT",
+        "page_icon": "📄",
+        "title": "DocumentGPT",
+        "markdown": load_markdown("./markdowns/document_gpt.md"),
+        "history_file_path": "./.cache/chat_history/history.json",
+        "prompt": chat_model.prompt,
+        "llm": chat_model.llm,
+        "chat_session_args": {
+            "_file_path": "files",
+            "_cache_dir": "embeddings",
+            "_embeddings": OpenAIEmbeddings(),
+        },
+    }
+
+    intro(**intro_config)
 
 
 def main() -> None:
-    app = App()
-    app.run()
+    chat_model = configure_chat_model()
+    run_chat_session(chat_model)
 
 
 if __name__ == "__main__":
