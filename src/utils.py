a36061a165e3af49dffb88e512b2ea811af00e9a
xshapira <max@winoutt.com>
2023-12-25 17:14:50 +0200

    add docstrings for functions load_txt, load_markdown, and load_json

--- src/utils.py
+++ src/utils.py
@@ -2,15 +2,42 @@ import json
 
 
 def load_txt(path):
+    """
+    Loads a text file and returns its contents as a string.
+
+    Args:
+        file_path (str): The path to the text file to be loaded.
+
+    Returns:
+        str: The contents of the text file.
+    """
     with open(path) as fp:
         return fp.read()
 
 
 def load_markdown(path):
+    """
+    Loads a Markdown file and returns its contents as a string.
+
+    Args:
+        file_path (str): The path to the Markdown file to be loaded.
+
+    Returns:
+        str: The contents of the Markdown file.
+    """
     with open(path) as fp:
         return fp.read()
 
 
 def load_json(path):
+    """
+    Loads a JSON file and returns its contents as a dictionary.
+
+    Args:
+        file_path (str): The path to the JSON file to be loaded.
+
+    Returns:
+        dict: The contents of the JSON file as a dictionary.
+    """
     with open(path) as fp:
         return json.load(fp)
