5237696b362adbf6e4bf88c5688a0108361d1da7
xshapira <max@winoutt.com>
2023-12-25 12:31:13 +0200

    update chat_session_manager module to use st.session_state

--- src/chat_session_manager.py
+++ src/chat_session_manager.py
@@ -6,29 +6,29 @@ from langchain.schema import messages_from_dict, messages_to_dict
 from src.utils import load_json
 
 
-def save_message(self, message, role):
-    self.messages.append({"message": message, "role": role})
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
 
 
-def send_message(self, message, role, save=True):
+def send_message(message, role, save=True):
     with st.chat_message(role):
         st.markdown(message)
     if save:
-        self.save_message(message, role)
+        save_message(message, role)
 
 
-def display_chat_history(self):
-    for message in self.messages:
-        self.send_message(message["message"], message["role"], save=False)
+def display_chat_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
 
 
-def save_history(_self, input, output):
-    _self.chat_history.append(
+def save_history(input, output):
+    st.session_state["chat_history"].append(
         {"input": input, "output": output},
     )
 
 
-def save_history_to_file(self, history_file_path):
+def save_history_to_file(history_file_path):
     history = st.session_state["memory"].chat_memory.messages
     history = messages_to_dict(history)
 
@@ -36,8 +36,8 @@ def save_history_to_file(self, history_file_path):
         json.dump(history, fp, indent=2)
 
 
-def restore_history_from_memory(self):
-    for history in self.chat_history:
+def restore_history_from_memory():
+    for history in st.session_state["chat_history"]:
         st.session_state["memory"].save_context(
             {"input": history["input"]},
             {"output": history["output"]},
@@ -45,7 +45,7 @@ def restore_history_from_memory(self):
 
 
 @st.cache_data(show_spinner="Loading history from file...")
-def load_history_from_file(_self, history_file_path):
+def load_history_from_file(history_file_path):
     loaded_message = load_json(history_file_path)
     history = messages_from_dict(loaded_message)
     st.session_state["memory"].chat_memory.messages = history
