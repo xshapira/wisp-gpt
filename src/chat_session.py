542d484bc89bc22de637ff1ef110a092c6627247
xshapira <max@winoutt.com>
2023-12-25 11:44:46 +0200

    move manage_chat_session() and format_docs() into separate chat_session.py file

---
+++ src/chat_session.py
@@ -0,0 +1,48 @@
+from operator import itemgetter
+
+import streamlit as st
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+
+from utils import (
+    Embedder,
+    display_chat_history,
+    restore_history_from_memory,
+    save_history,
+    save_history_to_file,
+    send_message,
+)
+
+
+def format_docs(docs):
+    return "/n/n".join(document.page_content for document in docs)
+
+
+def manage_chat_session(file, prompt, llm, history_file_path, **kwargs):
+    retriever = Embedder.embed_file(file, **kwargs)
+    send_message("I'm ready! Ask away.", "ai", save=False)
+    restore_history_from_memory()
+    display_chat_history()
+
+    message = st.chat_input("Ask me any question about your file")
+    if message:
+        send_message(message, "human")
+        chain = (
+            {
+                "context": retriever | RunnableLambda(format_docs),
+                "question": RunnablePassthrough(),
+            }
+            | RunnablePassthrough.assign(
+                chat_history=RunnableLambda(
+                    st.session_state["memory"].load_memory_variables
+                )
+                | itemgetter("chat_history")
+            )
+            | prompt
+            | llm
+        )
+        with st.chat_message("ai"):
+            result = chain.invoke(message)
+            save_history(message, result.content)
+
+        if len(st.session_state["memory"].chat_memory.messages) != 0:
+            save_history_to_file(history_file_path)
