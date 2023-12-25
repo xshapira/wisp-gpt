d24bccdd3c9b5340d4340191bc42624f24a015ac
xshapira <max@winoutt.com>
2023-12-25 18:42:46 +0200

    add docstrings for format_docs() and manage_chat_session()

--- src/chat_session.py
+++ src/chat_session.py
@@ -14,10 +14,37 @@ from src.embedder import Embedder
 
 
 def format_docs(docs):
+    """
+    Takes a list of documents and returns a string with the page content of
+    each document separated by two new lines.
+
+    Args:
+        docs: A list of documents. Each document has a property called "page_content" which contains the content of the document.
+
+    Returns:
+        str: A string containing the page content of each document in the input list.
+    """
     return "/n/n".join(document.page_content for document in docs)
 
 
 def manage_chat_session(file, prompt, llm, history_file_path, **kwargs):
+    """
+     Manages a chat session.
+
+     It is responsible for the following (probably too many things):
+        - Embedding a file
+        - Displaying chat history
+        - Receiving user input
+        - Processing user input by chaining functions
+        - Saving chat history
+
+    Args:
+        file: The path to the file containing the data you want to use for
+        the chat session. It could be a text file, a PDF file, or a DOCX file.
+        prompt: The prompt template for the chat model.
+        llm: The language model that will be used for generating responses in the chat session.
+        history_file_path: The file path where the chat history will be saved.
+    """
     retriever = Embedder.embed_file(file, **kwargs)
     send_message("I'm ready! Ask away.", "ai", save=False)
     restore_history_from_memory()
