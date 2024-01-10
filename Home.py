a693e7769f43f8ec4f3b152b32a91deda2d87e26
xshapira <max@winoutt.com>
2024-01-10 15:55:27 +0200

    move home markdown text into markdowns separate folder to improve structure

--- Home.py
+++ Home.py
@@ -1,35 +1,17 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="WispGPT",
-    page_icon="🤖",
-)
-
-st.markdown(
-    """
-# Hello!
-
-### Welcome to WispGPT - your personal AI sidekick!
-
-&nbsp;
-
-##### I'm here to empower your potential with AI superpowers at your fingertips:
+from src.utils import load_file
 
 
-- [📃 DocumentGPT](/DocumentGPT) - Unlock insights from your documents instantly with analysis and summarization
+def main() -> None:
+    st.set_page_config(
+        page_title="WispGPT",
+        page_icon="🤖",
+    )
 
-- [🔒 PrivateGPT](/PrivateGPT) - Feel secure sharing sensitive documents for confidential insights unique to you
+    markdown = load_file("./markdowns/home.md")
+    st.markdown(markdown)
 
-- [❓ QuizGPT](/QuizGPT) - Learn anything faster by generating custom quizzes
 
-- [🖥️ SiteGPT](/SiteGPT) - Stay data-driven by extracting key information from any website
-
-- [💼 MeetingGPT](/MeetingGPT) -  Never miss a detail with generated meeting summaries and ability to ask questions
-
-- [📈 InvestorGPT](/InvestorGPT) - Get insights on any public company with financial analysis and news reports
-
-&nbsp;
-
-###### Ready to give me a try? Pick one of my superpowers to start using me now!
-"""
-)
+if __name__ == "__main__":
+    main()
