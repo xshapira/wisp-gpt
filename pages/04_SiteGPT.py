1fb03fea9ead855fe1861058bb2c311cc2203dcf
Max Shapira <max@winoutt.com>
2024-01-16 11:14:14 +0200

    implement SiteGPT customer support agent to query website content

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,3 +1,162 @@
-import streamlit as st
+import re
 
-st.title("SiteGPT")
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import SitemapLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+
+from src.utils import load_file
+
+llm = ChatOpenAI(
+    temperature=0.1,
+)
+
+answers_messages = [
+    SystemMessagePromptTemplate.from_template(
+        load_file("./prompt_templates/site_gpt/system_message_answers.txt")
+    ),
+    HumanMessagePromptTemplate.from_template("{question}"),
+]
+
+choose_messages = [
+    SystemMessagePromptTemplate.from_template(
+        load_file("./prompt_templates/site_gpt/system_message.txt")
+    ),
+    HumanMessagePromptTemplate.from_template("{question}"),
+]
+
+answers_prompt = ChatPromptTemplate.from_messages(messages=answers_messages)
+choose_prompt = ChatPromptTemplate.from_messages(messages=choose_messages)
+
+
+def get_answers(input):
+    docs = input["context"]
+    question = input["question"]
+    answers_chain = answers_prompt | llm
+    result = {
+        "question": question,
+        "answers": [
+            {
+                "answer": answers_chain.invoke(
+                    {
+                        "question": question,
+                        "context": doc.page_content,
+                    }
+                ).content,
+                "source": doc.metadata["source"],
+                "date": doc.metadata["lastmod"],
+            }
+            for doc in docs
+        ],
+    }
+    return result
+
+
+def choose_answer(input):
+    answers = input["answers"]
+    question = input["question"]
+    choose_chain = choose_prompt | llm
+    shortened = "\n\n".join(
+        f"{answer['answer']}\nSource: {answer['source']}\n(Date: {answer['date']})\n"
+        for answer in answers
+    )
+    return choose_chain.invoke(
+        {
+            "question": question,
+            "answers": shortened,
+        },
+    )
+
+
+def parse_page(soup):
+    """
+    Currently using this to scrape the OpenAI site map.
+    """
+    header = soup.find("header")
+    footer = soup.find("footer")
+    author_articles_regex = r"Authors(\w+\s\w*View all articles)+"
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    text = (
+        str(soup.get_text())
+        .replace("\n", " ")
+        .replace("\xa0", " ")
+        .replace("CloseSearch Submit Blog", " ")
+    )
+    return re.sub(author_articles_regex, " ", text)
+
+
+@st.cache_data(show_spinner="Loading website...")
+def load_website(url):
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    loader = SitemapLoader(
+        url,
+        # filter_urls=[
+        #     r"^(.*\/blog\/).*",
+        # ],
+        parsing_function=parse_page,
+    )
+    # Set slower request rate to prevent blocking
+    # Default is 2 requests per second
+    loader.requests_per_second = 1
+    docs = loader.load_and_split(text_splitter=splitter)
+    vector_store = FAISS.from_documents(docs, OpenAIEmbeddings())
+    return vector_store.as_retriever()
+
+
+st.set_page_config(
+    page_title="SiteGPT",
+    page_icon="🖥️",
+)
+
+
+st.markdown(
+    """
+    # SiteGPT
+
+    Ask questions about the content of a website.
+
+    Start by writing the URL of the website in the sidebar.
+    """
+)
+
+
+with st.sidebar:
+    url = st.text_input(
+        "Enter a URL:",
+        placeholder="https://example.com",
+    )
+
+
+if url:
+    if ".xml" not in url:
+        with st.sidebar:
+            st.error("The URL must end with .xml")
+    else:
+        retriever = load_website(url)
+        query = st.text_input("Ask a question about the content of the website.")
+        if query:
+            chain = (
+                {
+                    "context": retriever,
+                    "question": RunnablePassthrough(),
+                }
+                | RunnableLambda(get_answers)
+                | RunnableLambda(choose_answer)
+            )
+
+            result = chain.invoke(query)
+            st.write(result.content.replace("$", r"\$"))
