7f476ef082e899f513971fb3b0ea0cd0c30fd5c8
Max Shapira <max@winoutt.com>
2024-02-04 12:20:43 +0200

    implement interactive summary generation and transcript viewing in separate tabs

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -5,9 +5,18 @@ from pathlib import Path
 
 import openai
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from pydub import AudioSegment
 
 from src.utils import load_file
+
+llm = ChatOpenAI(
+    temperature=0.1,
+)
 
 has_transcription = Path("./.cache/podcast.txt").exists()
 
@@ -86,7 +95,7 @@ with st.sidebar:
     )
 
 if video:
-    with st.status("Loading video..."):
+    with st.status("Loading video...") as status:
         video_content = video.read()
         video_path = f"./.cache/{video.name}"
         audio_path = video_path.replace(".mp4", ".mp3")
@@ -94,12 +103,75 @@ if video:
         transcript_dir = video_path.replace(".mp4", ".txt")
         with open(video_path, "wb") as fp:
             fp.write(video_content)
-    with st.status("Extracting audio from video..."):
+        status.update(label="Extracting audio from video...")
         extract_audio_from_video(video_path)
-    with st.status("Cutting audio segments..."):
+        status.update(label="Cutting audio segments...")
         cut_audio_in_chunks(audio_path, chunk_size=10, chunks_dir=chunks_dir)
-    with st.status("Transcribing audio..."):
+        status.update(label="Transcribing audio...")
         transcribe_audio_chunks(
             chunks_dir=chunks_dir,
             destination=transcript_dir,
         )
+
+    transcript_tab, summary_tab, qa_tab = st.tabs(
+        [
+            "Transcript",
+            "Summary",
+            "Q&A",
+        ],
+    )
+
+    with transcript_tab:
+        with open(transcript_dir) as file:
+            st.write(file.read())
+
+    with summary_tab:
+        start = st.button("Generate summary")
+
+        if start:
+            loader = TextLoader("./.cache/podcast.txt")
+            splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+                chunk_size=800,
+                chunk_overlap=100,
+            )
+            docs = loader.load_and_split(text_splitter=splitter)
+
+            first_summary_prompt = ChatPromptTemplate.from_template(
+                """
+                Write a concise summary of the following:
+                "{text}"
+                CONCISE SUMMARY:
+                """
+            )
+            first_summary_chain = first_summary_prompt | llm | StrOutputParser()
+            summary = first_summary_chain.invoke(
+                {
+                    "text": docs[0].page_content,
+                }
+            )
+            refine_prompt = ChatPromptTemplate.from_template(
+                """
+                Your job is to produce a final summary.
+                We have provided an existing summary up to a certain point: {existing_summary}
+                We have the opportunity to refine the existing summary (only if needed) with some more context below.
+                ------------
+                {context}
+                ------------
+                Given the new context, refine the original summary.
+                If the context isn't useful, RETURN the original summary.
+                """
+            )
+            refine_chain = refine_prompt | llm | StrOutputParser()
+
+            with st.status("Summarizing...") as status:
+                for i, doc in enumerate(docs[1:], start=1):
+                    status.update(
+                        label=f"Generating summary for {i}/{len(docs) - 1}",
+                    )
+                    summary = refine_chain.invoke(
+                        {
+                            "existing_summary": summary,
+                            "context": doc.page_content,
+                        }
+                    )
+                    st.write(summary)
