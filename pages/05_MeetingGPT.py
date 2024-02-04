70045de2f2c6aaea5f327c502adeff9c0598cc6b
Max Shapira <max@winoutt.com>
2024-02-04 10:14:39 +0200

    add cut_audio_in_chunks() to split audio into timed chunks

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -1,6 +1,8 @@
+import math
 import subprocess
 
 import streamlit as st
+from pydub import AudioSegment
 
 from src.utils import load_file
 
@@ -21,6 +23,19 @@ def extract_audio_from_video(video_path):
     subprocess.run(command)
 
 
+def cut_audio_in_chunks(audio_path, chunk_size, chunks_dir):
+    track = AudioSegment.from_mp3(audio_path)
+    chunk_length = chunk_size * 60 * 1000
+    chunks = math.ceil(len(track) / chunk_length)
+
+    for i in range(chunks):
+        start_time = i * chunk_length
+        end_time = (i + 1) * chunk_length
+        print(f"start: {start_time}, end: {end_time}")
+        chunk = track[start_time:end_time]
+        chunk.export(f"{chunks_dir}/chunk_{i}.mp3", format="mp3")
+
+
 markdown_file = load_file("./markdowns/meeting_gpt.md")
 st.set_page_config(
     page_title="MeetingGPT",
@@ -37,6 +52,10 @@ with st.sidebar:
     if video:
         video_content = video.read()
         video_path = f"./.cache/{video.name}"
+        audio_path = video_path.replace(".mp4", ".mp3")
         with open(video_path, "wb") as fp:
             fp.write(video_content)
         extract_audio_from_video(video_path)
+        cut_audio_in_chunks(
+            audio_path, chunk_size=10, chunks_dir="./.cache/audio_chunks"
+        )
